2c83c0db608f39545e18e2a2200355281092dd98
Saransh <dassaransh@gmail.com>
2021-09-01 20:11:12 +0530

    Version 1.0

---
+++ tiktok-backend/data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    url: "https://www.instagram.com/reel/CKtmNuZFpWP/?utm_source=ig_web_copy_link",
+    channel: "Saransh",
+    song: "99-Problems but React aint 1",
+    likes: "5123",
+    messages: "2616",
+    description: "TIKTOK Clone by DEV Saransh",
+    shares: "1726",
+  },
+  {
+    url: "https://www.instagram.com/reel/CPe2JDwFzc4/?utm_source=ig_web_copy_link",
+    channel: "Magician",
+    song: "Coffee makes me Happy",
+    likes: "55123",
+    messages: "24616",
+    description: "TIKTOK Clone by DEV Saransh",
+    shares: "12726",
+  },
+];
+
+export default data;
---
+++ tiktok-backend/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import mongoose from "mongoose";
+import Data from "./data.js";
+import Videos from "./dbModel.js";
+
+//app config
+const app = express();
+const port = process.env.PORT || 9000;
+//middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"),
+    res.setHeader("Access-Control-Allow-Headers", "*"),
+    next();
+});
+//DB config
+const connection_url =
+  "mongodb+srv://admin:TB2qoMOSlhBGrQXa@cluster0.oahyi.mongodb.net/tiktok?retryWrites=true&w=majority";
+
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//api endpoints
+app.get("/", (req, res) => {
+  res.status(200).send("Server Connected");
+});
+
+app.get("/v1/posts", (req, res) => res.status(200).send(Data));
+
+app.get("/v2/posts", (req, res) => {
+  Videos.find({}, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/v2/posts", (req, res) => {
+  //Post request is to Add data to the database
+  const dbVideos = req.body;
+
+  Videos.create(dbVideos, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+//Listener
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
